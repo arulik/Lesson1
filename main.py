6f56c3962c4cc96e507138440daf163f1f2e7ef1
arulik <arulik@icloud.com>
2022-11-24 16:06:29 +0200

    prod new2

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-
-
-#first_line = "* * * * *\t*\t\t*\t* * * * *\t*\t\t\t\t*\t\t*\t\t*"
-#second_line = '*\t\t*\t*\t\t*\t*\t\t\t*\t\t\t   * *\t\t* *\t\t*'
-#third_line = "* * * * *\t*\t\t*\t* * * * *\t*\t\t\t  *   *\t\t*\t*\t*"
-#fourth_line = "* \t*\t\t*\t\t*\t\t\t*\t*\t\t\t *\t*  *\t*\t  * *"
-#fifth_line = "*\t\t*\t* * * * *\t* * * * *\t* * * * *\t*\t\t*\t*\t    *"
-#print(first_line)
-#print(second_line)
-#print(third_line)
-#print(fourth_line)
-#print(fifth_line)
+# first_line = "* * * * *\t*\t\t*\t* * * * *\t*\t\t\t\t*\t\t*\t\t*"
+# second_line = '*\t\t*\t*\t\t*\t*\t\t\t*\t\t\t   * *\t\t* *\t\t*'
+# third_line = "* * * * *\t*\t\t*\t* * * * *\t*\t\t\t  *   *\t\t*\t*\t*"
+# fourth_line = "* \t*\t\t*\t\t*\t\t\t*\t*\t\t\t *\t*  *\t*\t  * *"
+# fifth_line = "*\t\t*\t* * * * *\t* * * * *\t* * * * *\t*\t\t*\t*\t    *"
+# print(first_line)
+# print(second_line)
+# print(third_line)
+# print(fourth_line)
+# print(fifth_line)
 
 RUSLAN = "* * * * *\t*\t\t*\t* * * * *\t*\t\t\t\t*\t\t*\t\t*\n*\t\t*\t*\t\t*\t*\t\t\t*\t\t\t   * *\t\t* *\t\t*\n* * * * *\t*\t\t*\t* * * * *\t*\t\t\t  *   *\t\t*\t*\t*\n* \t*\t\t*\t\t*\t\t\t*\t*\t\t\t *\t*  *\t*\t  * *\n*\t\t*\t* * * * *\t* * * * *\t* * * * *\t*\t\t*\t*\t    *"
 print(RUSLAN)
 
+Bell = " \\a\t\tBell (alert)\n"
+Backspace = ("\\b\t\tBackspace")
+print(
+    "\\a\t\tBell (alert)\n\\b\t\tBackspace\n\\n\t\tNew line\n\\t\t\tHorizontal tab\n\\\ \t\tBackslash \\\n\\\"\t\tDouble quotation mark \"\n\\\'\t\tSingle quotation mark \'")
 
-print("\\a\t\tBell (alert)\n\\b\t\tBackspace\n\\n\t\tNew line\n\\t\t\tHorizontal tab\n\\\ \t\tBackslash \\\n\\\"\t\tDouble quotation mark \"\n\\\'\t\tSingle quotation mark \'")
+print(Bell,Backspace)
