5d7a6ec1ec49fd48bd73c850fdb59f85823e3b42
arulik <arulik@icloud.com>
2022-11-24 15:48:16 +0200

    prod new

--- main.py
+++ main.py
@@ -1,39 +1,12 @@
-# star = "*"
-# print(star,star,star,star," ",star)
-# print(star,"   ",star," ",star)
-# print(star,star,star,star," ",star)
-# print(star,"  ",star,"  ",star)
-# print(star,"   ",star," ",star)
 
-first_line = "* * * * *\t*\t\t*\t* * * * *\t*\t\t\t\t*\t\t\t*\t\t*"
-second_line = '*\t\t*\t*\t\t*\t*\t\t\t*\t\t\t   * *\t\t\t* *\t\t*'
-third_line = "* * * * *\t*\t\t*\t* * * * *\t*\t\t\t  *   *\t\t\t*\t*\t*"
-fourth_line = "* \t*\t\t*\t\t*\t\t\t*\t*\t\t\t *\t*  *\t\t*\t  * *"
-fifth_line = "*\t\t*\t* * * * *\t* * * * *\t* * * * *\t*\t\t*\t\t*\t    *"
+first_line = "* * * * *\t*\t\t*\t* * * * *\t*\t\t\t\t*\t\t*\t\t*"
+second_line = '*\t\t*\t*\t\t*\t*\t\t\t*\t\t\t   * *\t\t* *\t\t*'
+third_line = "* * * * *\t*\t\t*\t* * * * *\t*\t\t\t  *   *\t\t*\t*\t*"
+fourth_line = "* \t*\t\t*\t\t*\t\t\t*\t*\t\t\t *\t*  *\t*\t  * *"
+fifth_line = "*\t\t*\t* * * * *\t* * * * *\t* * * * *\t*\t\t*\t*\t    *"
 print(first_line)
 print(second_line)
 print(third_line)
 print(fourth_line)
 print(fifth_line)
 
-
-
-# letter_R = "* * * * *\n*\t\t*\n* * * * *\n*\t*\n*\t *\n*     *"
-# letter_U = "*\t\t*"
-# print(letter_R)
-
-
-# s = star, x = spase
-
-# sssss = "* * * * *\t"
-# sxxxs = "*\t\t*\t"
-# ssxxx = "* * \t\t"
-# sxsxx = "*\t *\t\t"
-# sxxsx = "*\t  *\t\t"
-# one_star = "*"
-# print(sssss,sxxxs)
-# print(sxxxs,sxxxs)
-# print(sssss,sxxxs)
-# print(ssxxx,sxxxs)
-# print(sxsxx,sxxxs)
-# print(sxxsx,sssss)
