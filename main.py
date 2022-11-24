305c8d02c24d718d9bf34140b1f0cd0606a65bfb
arulik <arulik@icloud.com>
2022-11-24 15:51:46 +0200

    prod new2

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-first_line = "* * * * *\t*\t\t*\t* * * * *\t*\t\t\t\t*\t\t*\t\t*"
-second_line = '*\t\t*\t*\t\t*\t*\t\t\t*\t\t\t   * *\t\t* *\t\t*'
-third_line = "* * * * *\t*\t\t*\t* * * * *\t*\t\t\t  *   *\t\t*\t*\t*"
-fourth_line = "* \t*\t\t*\t\t*\t\t\t*\t*\t\t\t *\t*  *\t*\t  * *"
-fifth_line = "*\t\t*\t* * * * *\t* * * * *\t* * * * *\t*\t\t*\t*\t    *"
+#first_line = "* * * * *\t*\t\t*\t* * * * *\t*\t\t\t\t*\t\t*\t\t*"
+#second_line = '*\t\t*\t*\t\t*\t*\t\t\t*\t\t\t   * *\t\t* *\t\t*'
+#third_line = "* * * * *\t*\t\t*\t* * * * *\t*\t\t\t  *   *\t\t*\t*\t*"
+#fourth_line = "* \t*\t\t*\t\t*\t\t\t*\t*\t\t\t *\t*  *\t*\t  * *"
+#fifth_line = "*\t\t*\t* * * * *\t* * * * *\t* * * * *\t*\t\t*\t*\t    *"
 #print(first_line)
 #print(second_line)
 #print(third_line)
 #print(fourth_line)
 #print(fifth_line)
 
-all_lines = "* * * * *\t*\t\t*\t* * * * *\t*\t\t\t\t*\t\t*\t\t*\n*\t\t*\t*\t\t*\t*\t\t\t*\t\t\t   * *\t\t* *\t\t*\n* * * * *\t*\t\t*\t* * * * *\t*\t\t\t  *   *\t\t*\t*\t*\n* \t*\t\t*\t\t*\t\t\t*\t*\t\t\t *\t*  *\t*\t  * *\n*\t\t*\t* * * * *\t* * * * *\t* * * * *\t*\t\t*\t*\t    *"
+RUSLAN = "* * * * *\t*\t\t*\t* * * * *\t*\t\t\t\t*\t\t*\t\t*\n*\t\t*\t*\t\t*\t*\t\t\t*\t\t\t   * *\t\t* *\t\t*\n* * * * *\t*\t\t*\t* * * * *\t*\t\t\t  *   *\t\t*\t*\t*\n* \t*\t\t*\t\t*\t\t\t*\t*\t\t\t *\t*  *\t*\t  * *\n*\t\t*\t* * * * *\t* * * * *\t* * * * *\t*\t\t*\t*\t    *"
 print(all_lines)
