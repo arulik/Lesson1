abbb28258bcf09aeb2de82b2c1012d827b4f340a
arulik <arulik@icloud.com>
2022-11-24 15:38:40 +0200

    lines name

--- main.py
+++ main.py
@@ -5,27 +5,32 @@
 # print(star,"  ",star,"  ",star)
 # print(star,"   ",star," ",star)
 
-# first_line = "* * * * *\t*\t\t\t* * * * *\t*\t\t*\t\t* \t* \t*"
-# second_line = '*\t\t*\n* * * * *\n*\n*'
-# print(first_line)
-# print(second_line)
-
+first_line = "* * * * *\t*\t\t*\t* * * * *\t*\t\t\t\t*\t\t\t\t*\t\t\t*"
+second_line = '*\t\t*\t*\t\t*\t*\t\t\t*\t\t\t   * *\t\t\t\t* *\t\t\t*'
+third_line = "* * * * *\t*\t\t*\t* * * * *\t*\t\t\t  * * *\t\t\t\t*\t*\t\t*"
+fourth_line = "* \t*\t\t*\t\t*\t\t\t*\t*\t\t\t *\t   *\t\t\t*\t  *  \t*"
+fifth_line = ""
+sixth_line = ""
+print(first_line)
+print(second_line)
+print(third_line)
+print(fourth_line)
 # letter_R = "* * * * *\n*\t\t*\n* * * * *\n*\t*\n*\t *\n*     *"
 # letter_U = "*\t\t*"
-# print(letter_R,letter_U)
+# print(letter_R)
 
 
 # s = star, x = spase
 
-sssss = "* * * * *\t"
-sxxxs = "*\t\t*\t"
-ssxxx = "* * \t\t"
-sxsxx = "*\t *\t\t"
-sxxsx = "*\t  *\t\t"
-one_star = "*"
-print(sssss,sxxxs)
-print(sxxxs,sxxxs)
-print(sssss,sxxxs)
-print(ssxxx,sxxxs)
-print(sxsxx,sxxxs)
-print(sxxsx,sssss)
+# sssss = "* * * * *\t"
+# sxxxs = "*\t\t*\t"
+# ssxxx = "* * \t\t"
+# sxsxx = "*\t *\t\t"
+# sxxsx = "*\t  *\t\t"
+# one_star = "*"
+# print(sssss,sxxxs)
+# print(sxxxs,sxxxs)
+# print(sssss,sxxxs)
+# print(ssxxx,sxxxs)
+# print(sxsxx,sxxxs)
+# print(sxxsx,sssss)
