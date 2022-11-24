d5d1516c9f56cdc31cbad5bc72b9983cc4328422
arulik <arulik@icloud.com>
2022-11-24 16:17:13 +0200

    DZ2

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ RUSLAN = "* * * * *\t*\t\t*\t* * * * *\t*\t\t\t\t*\t\t*\t\t*\n*\t\t*\t*\t\t*\t*\
          "  *   *\t\t*\t*\t*\n* \t*\t\t*\t\t*\t\t\t*\t*\t\t\t *\t*  *\t*\t  * *\n*\t\t*\t* * * * *\t* * * * *\t* * * * *\t*\t\t*\t*\t    *"
 print(RUSLAN)
 
-Bell = "\\a\t\tBell (alert)\n"
-Backspace = ("\\b\t\tBackspace\n")
-New_line="\\n\t\tNew line"
+Bell = "\\a\t\tBell (alert)"
+Backspace = "\n\\b\t\tBackspace"
+New_line="\n\\n\t\tNew line"
 
 #print("\\a\t\tBell (alert)\n\\b\t\tBackspace\n\\n\t\tNew line\n\\t\t\tHorizontal tab\n\\\ \t\tBackslash \\\n\\\"\t\tDouble quotation mark \"\n\\\'\t\tSingle quotation mark \'")
 
 print(Bell,Backspace,New_line)
+
+
+print("'test'")
