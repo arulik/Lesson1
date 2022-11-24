dc515753b687e8ac8ffa7d4cdc9b8f29858298c3
arulik <arulik@icloud.com>
2022-11-24 16:51:59 +0200

    DZ3

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-import antigravity
-print("https://xkcd.com/353/")
+# First task
+#from __future__ import braces
+print("not a chance")
+
+import antigravity    #https://xkcd.com/353/
 
 import __hello__
 
-print("from __future__ import braces")
 
+# Second task
 
 RUSLAN = "* * * * *\t*\t\t*\t* * * * *\t*\t\t\t\t*\t\t*\t\t*\n*\t\t*\t*\t\t*\t*\t\t\t*\t\t\t   * *\t\t* *\t\t*\n* * * * *\t*\t\t*\t* * * * *\t*\t\t\t" \
          "  *   *\t\t*\t*\t*\n* \t*\t\t*\t\t*\t\t\t*\t*\t\t\t *\t*  *\t*\t  * *\n*\t\t*\t* * * * *\t* * * * *\t* * * * *\t*\t\t*\t*\t    *"
 print(RUSLAN)
+
+# Third task
 
 Bell = "\n\\a\t\tBell (alert)"
 Backspace = "\n\\b\t\tBackspace"
@@ -18,3 +23,5 @@ Backslash = "\n\\ \t\tBackslash \\"
 Double_quotation_mark="\n\\\"\t\tDouble quotation mark \""
 Single_quotation_mark="\n\\\'\t\tSingle quotation mark \'"
 print("Escape sequences", Bell, Backspace, New_line, Horizontal_tab,Backslash,Double_quotation_mark,Single_quotation_mark)
+
+
