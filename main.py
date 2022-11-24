7e44beb2a239f03945201be7728163aabc7ee432
arulik <arulik@icloud.com>
2022-11-24 14:22:14 +0200

    new idea with lines

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-#star = "*"
-#print(star,star,star,star," ",star)
-#print(star,"   ",star," ",star)
-#print(star,star,star,star," ",star)
-#print(star,"  ",star,"  ",star)
-#print(star,"   ",star," ",star)
+# star = "*"
+# print(star,star,star,star," ",star)
+# print(star,"   ",star," ",star)
+# print(star,star,star,star," ",star)
+# print(star,"  ",star,"  ",star)
+# print(star,"   ",star," ",star)
 
-first_line = "* * * * *\t*\t\t\t* * * * *\t*\t\t*\t\t* \t* \t*"
-second_line = '*\t\t*\n* * * * *\n*\n*'
-print(first_line)
-print(second_line)
+# first_line = "* * * * *\t*\t\t\t* * * * *\t*\t\t*\t\t* \t* \t*"
+# second_line = '*\t\t*\n* * * * *\n*\n*'
+# print(first_line)
+# print(second_line)
 
+# letter_R = "* * * * *\n*\t\t*\n* * * * *\n*\t*\n*\t *\n*     *"
+# letter_U = "*\t\t*"
+# print(letter_R,letter_U)
 
+line1 = "* * * * *\t"
+line2 = "*\t\t*"
+print(line1, line2)
+print(line2)
