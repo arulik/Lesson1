657f063319a5d717e8ed9e248bedafc7c47d6796
arulik <arulik@icloud.com>
2022-11-24 16:39:35 +0200

    DZ2

--- main.py
+++ main.py
@@ -5,16 +5,11 @@ RUSLAN = "* * * * *\t*\t\t*\t* * * * *\t*\t\t\t\t*\t\t*\t\t*\n*\t\t*\t*\t\t*\t*\
          "  *   *\t\t*\t*\t*\n* \t*\t\t*\t\t*\t\t\t*\t*\t\t\t *\t*  *\t*\t  * *\n*\t\t*\t* * * * *\t* * * * *\t* * * * *\t*\t\t*\t*\t    *"
 print(RUSLAN)
 
-
-
-
 Bell = "\n\\a\t\tBell (alert)"
 Backspace = "\n\\b\t\tBackspace"
-New_line="\n\\n\t\tNew line"
-Horizontal_tab="\n\\t\t\tHorizontal tab"
-
-
-print("Escape sequences",Bell,Backspace,New_line,Horizontal_tab)
-
-
-
+New_line = "\n\\n\t\tNew line"
+Horizontal_tab = "\n\\t\t\tHorizontal tab"
+Backslash = "\n\\ \t\tBackslash \\"
+Double_quotation_mark="\n\\\"\t\tDouble quotation mark \""
+Single_quotation_mark="\n\\\'\t\tSingle quotation mark \'"
+print("Escape sequences", Bell, Backspace, New_line, Horizontal_tab,Backslash,Double_quotation_mark,Single_quotation_mark)
