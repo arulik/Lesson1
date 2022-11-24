db423161ab15bfee4fa9d829ac29808a280e138f
arulik <arulik@icloud.com>
2022-11-24 13:59:18 +0200

    Line method, first letter

--- main.py
+++ main.py
@@ -5,8 +5,9 @@
 #print(star,"  ",star,"  ",star)
 #print(star,"   ",star," ",star)
 
-first_line = "*****\t*\t\t\t*****\t*\t\t*\t\t*\t*\t*"
-second_line = '*\t*\n* *\n*'
+first_line = "* * * * *\t*\t\t\t* * * * *\t*\t\t*\t\t* \t* \t*"
+second_line = '*\t\t*\n*\n*\n*'
 print(first_line)
 print(second_line)
+
 
