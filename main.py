aa59396c02f157464b9bf11b06d766e17eb11aef
arulik <arulik@icloud.com>
2022-12-02 21:09:08 +0200

    DZ3

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
-# First task
-#from __future__ import braces
-print("not a chance")
+# schoolers = float(input('number of sch:'))
+# apples = float(input('num of apple:'))
 
-import antigravity    #https://xkcd.com/353/
+# print(f'{(apples / schoolers)}',type(apples / schoolers))
 
-import __hello__
-
-
-# Second task
-
-RUSLAN = "* * * * *\t*\t\t*\t* * * * *\t*\t\t\t\t*\t\t*\t\t*\n*\t\t*\t*\t\t*\t*\t\t\t*\t\t\t   * *\t\t* *\t\t*\n* * * * *\t*\t\t*\t* * * * *\t*\t\t\t" \
-         "  *   *\t\t*\t*\t*\n* \t*\t\t*\t\t*\t\t\t*\t*\t\t\t *\t*  *\t*\t  * *\n*\t\t*\t* * * * *\t* * * * *\t* * * * *\t*\t\t*\t*\t    *"
-print(RUSLAN)
-
-# Third task
-
-Bell = "\n\\a\t\tBell (alert)"
-Backspace = "\n\\b\t\tBackspace"
-New_line = "\n\\n\t\tNew line"
-Horizontal_tab = "\n\\t\t\tHorizontal tab"
-Backslash = "\n\\ \t\tBackslash \\"
-Double_quotation_mark="\n\\\"\t\tDouble quotation mark \""
-Single_quotation_mark="\n\\\'\t\tSingle quotation mark \'"
-print("Escape sequences", Bell, Backspace, New_line, Horizontal_tab,Backslash,Double_quotation_mark,Single_quotation_mark)
+a = int(input('a=:'))
+b = int(input('b=:'))
+x = a / b
+print("яблок у кожного школяра: " + str(int(x)))
+print("яблок в корзині: " + str((float(x)-int(x))*b))
 
 
