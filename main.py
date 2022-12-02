8015f5d227a7b8a3c680d69a291efba0fae447f4
arulik <arulik@icloud.com>
2022-12-02 21:19:11 +0200

    calc

--- main.py
+++ main.py
@@ -2,12 +2,19 @@
 # apples = float(input('num of apple:'))
 
 # print(f'{(apples / schoolers)}',type(apples / schoolers))
-
+'''
 a = int(input('a=:'))
 b = int(input('b=:'))
 x = a / b
 print("яблок у кожного школяра: " + str(int(x)))
 print("яблок в корзині: " + str((float(x)-int(x))*b))
+'''
+
+
+
+
+
+
 
 
 
