f104d6f61ceb01b6a961d45bbed46d9e0e50a53e
arulik <arulik@icloud.com>
2022-12-03 11:05:23 +0200

    first part dz done

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+'''
 print('*** Задача про школярів та яблока ***')
 apple = int(input('Введіть кількість яблок: '))
 people = int(input('Введіть кількість школярів: '))
@@ -5,8 +6,19 @@ per_people = apple // people
 bag = int(apple % people)
 print("яблок у кожного школяра: " + str(per_people))
 print("яблок в корзині: " + str(bag))
+'''
 
+num_of_class = 3
+first_class=int(input('Кількість дітей у першому класі: '))
+second_class=int(input('Кількість дітей у другому класі: '))
+third_class=int(input('Кількість дітей у третьому класі: '))
 
+x = int((first_class + second_class + third_class) / 2)
+y = first_class + second_class + third_class
+print(y)
+print(x)
+
+#print(f'{(first_class + second_class + third_class) / 2}')
 
 
 
