9e9481eac170b9b59d55fd8e4d5ae0f1975c28e4
arulik <arulik@icloud.com>
2022-12-03 10:25:39 +0200

    first part dz done

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
-# schoolers = float(input('number of sch:'))
-# apples = float(input('num of apple:'))
-
-# print(f'{(apples / schoolers)}',type(apples / schoolers))
-'''
-a = int(input('a=:'))
-b = int(input('b=:'))
-x = a / b
-print("яблок у кожного школяра: " + str(int(x)))
-print("яблок в корзині: " + str((float(x)-int(x))*b))
-'''
-
-
+print('*** Задача про школярів та яблока ***')
 apple = int(input('Введіть кількість яблок: '))
 people = int(input('Введіть кількість школярів: '))
 per_people = apple // people
 bag = int(apple % people)
-
 print("яблок у кожного школяра: " + str(per_people))
 print("яблок в корзині: " + str(bag))
+
+
+
 
 
 
