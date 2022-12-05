c9a205d7ceda9584a37547058b1460320b9e81e1
arulik <arulik@icloud.com>
2022-12-05 09:49:39 +0200

    Done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+'''
 print('*** Задача про школярів та яблока ***')
 apple = int(input('Введіть кількість яблок: '))
 people = int(input('Введіть кількість школярів: '))
@@ -6,17 +6,26 @@ per_people = apple // people
 bag = int(apple % people)
 print("яблок у кожного школяра: " + str(per_people))
 print("яблок в корзині: " + str(bag), end='\n\n')
-
+'''
 
 print('*** Задача про парти для учнів ***')
-first_class=int(input('Кількість дітей у першому класі: '))
-second_class=int(input('Кількість дітей у другому класі: '))
-third_class=int(input('Кількість дітей у третьому класі: '))
-main_part = int((first_class + second_class + third_class) // 2)
-secondary_part = int(first_class + second_class + third_class) % 2
-print(f'Кількість парт для покупки {main_part+secondary_part}', end='\n\n')
+first_class = int(input('Кількість дітей у першому класі: '))
+second_class = int(input('Кількість дітей у другому класі: '))
+third_class = int(input('Кількість дітей у третьому класі: '))
+main_part = int(first_class) // 2
+secondary_part = int(first_class) % 2
+main_part += int(second_class) // 2
+secondary_part += int(second_class) % 2
+main_part2 = int(third_class) // 2
+secondary_part2 = int(third_class) % 2
 
 
+print(total_first + total_second + total_third)
+
+# print(f'Кількість парт для покупки {main_part+secondary_part}', end='\n\n')
+# print(main_part)
+# print(secondary_part)
+'''
 first_num = int(input("Введіть число для інверсії: "))
 second_num = 0
 while first_num > 0:
@@ -35,4 +44,4 @@ min_value = sec_value // 60
 sec_value %= 60
 print(f'{hour_value}:{min_value}:{sec_value}')
 
-
+'''
