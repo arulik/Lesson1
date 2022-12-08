0bf1fd857cd34022b1422594f94ba32a10263a01
arulik <arulik@icloud.com>
2022-12-08 13:47:46 +0200

    numbers

--- lesson3.py
+++ lesson3.py
@@ -1,3 +1,4 @@
+'''
 first_num = float(input('Введіть перше число : '))
 second_num = float(input('Введіть друге число: '))
 operators = input('Що з ними зробити?: ')
@@ -13,4 +14,13 @@ elif operators == '*':
     print(first_num*second_num)
 elif operators == '**':
     print(first_num**second_num)
+'''
 
+number = int(input('Input number: '))
+
+#for number in range(start_number,number):
+#    print(number**2, end=' ')
+
+for i in range(1, number):
+    print(i**2, end=' ')
+print()
