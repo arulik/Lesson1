f1270c67ba540ffb42d44d26371f900e1fe72d50
arulik <arulik@icloud.com>
2022-12-13 15:59:32 +0200

    fourth task task

--- lesson4.py
+++ lesson4.py
@@ -1,29 +1,65 @@
-print('\n\n*** Трикутник А ***')
-user_num=int(input('Введіть висоту трикутника: '))
-number = user_num * 2
-for height in range(0, number, 2):
-    string = ''
-    for weight in range((number * 2) * 2):
-        if height == number-2:
-            if weight == 0:
-                string += '  *'
-            elif weight <= number-2:
-                string += ' *'
-        elif number - height == weight or number + height == weight:
-            string += '*'
-        else:
-            string += ' '
-    print(string)
+# print('\n\n*** Трикутник А ***')
+# user_num=int(input('Введіть висоту трикутника: '))
+# number = user_num * 2
+# for height in range(0, number, 2):
+#     string = ''
+#     for weight in range((number * 2) * 2):
+#         if height == number-2:
+#             if weight == 0:
+#                 string += '  *'
+#             elif weight <= number-2:
+#                 string += ' *'
+#         elif number - height == weight or number + height == weight:
+#             string += '*'
+#         else:
+#             string += ' '
+#     print(string)
+#
+# print('***Трикутник B ***')
+# number = int(input('Введіть висоту трикутника: '))
+# user_height = number * 2
+# for height in range(0, user_height, 2):
+#     for weigth in range(user_height - height):
+#         print(end=" ")
+#     for weight in range(height + 1):
+#         print("*", end=" ")
+#     print()
+#
+# print('***Трикутник C ***')
+# number = int(input('Введіть висоту трикутника: '))
+# user_height = number * 2
+# for height in range(0, user_height, 2):
+#     for weigth in range(user_height - height):
+#         print(end=" ")
+#     for weight in range(height + 1):
+#         print("*", end=" ")
+#     print()
+# number = user_height
+# for height in range(number,-2, -2):
+#     string = ''
+#     for weight in range((number * 2) * 2):
+#         if height == number:
+#             if weight == 0:
+#                 string += '*'
+#             elif weight <= number:
+#                 string += ' *'
+#         elif number - height == weight or number + height == weight:
+#             string += '*'
+#         else:
+#             string += ' '
+#     print(string)
+#
+# print('\n\n\n*** Генерація чисел***')
+# answer = input('Розпочати виконання програми? \nВведіть (y\\n): ')
+# if answer == 'y':
+#     my_string = '0123456789'
+#     for i in my_string:
+#         for j in my_string:
+#             print(int(i+j))
+# else:
+#     print('Дякую за увагу')
 
-print('***Трикутник B ***')
-number = int(input('Введіть висоту трикутника: '))
-user_height = number * 2
-for height in range(0, user_height, 2):
-    for weigth in range(user_height - height):
-        print(end=" ")
-    for weight in range(height + 1):
-        print("*", end=" ")
-    print()
+
 
 print('***Трикутник C ***')
 number = int(input('Введіть висоту трикутника: '))
@@ -38,6 +74,8 @@ number = user_height
 for height in range(number,-2, -2):
     string = ''
     for weight in range((number * 2) * 2):
+        if height+2 == weight:
+            string += '*'
         if height == number:
             if weight == 0:
                 string += '*'
@@ -48,17 +86,3 @@ for height in range(number,-2, -2):
         else:
             string += ' '
     print(string)
-
-print('\n\n\n*** Генерація чисел***')
-answer = input('Розпочати виконання програми? \nВведіть (y\\n): ')
-if answer == 'y':
-    my_string = '0123456789'
-    for i in my_string:
-        for j in my_string:
-            print(int(i+j))
-else:
-    print('Дякую за увагу')
-
-
-
-
