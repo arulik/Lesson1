74b4e2ea63f87469c90fc5f37640a4c9e69482e6
arulik <arulik@icloud.com>
2022-12-13 15:55:09 +0200

    third task done

--- lesson4.py
+++ lesson4.py
@@ -1,18 +1,12 @@
-# print('***Трикутник B ***')
-# number = int(input('Введіть висоту трикутника: '))
-# user_height = number * 2
-# for height in range(0, user_height, 2):
-#     for weigth in range(user_height - height):
-#         print(end=" ")
-#     for weight in range(height + 1):
-#         print("*", end=" ")
-#     print()
-#
-# print('\n\n\n*** Генерація чисел***')
-# my_string = '0123456789'
-# for i in my_string:
-#     for j in my_string:
-#         print(int(i+j))
+print('***Трикутник B ***')
+number = int(input('Введіть висоту трикутника: '))
+user_height = number * 2
+for height in range(0, user_height, 2):
+    for weigth in range(user_height - height):
+        print(end=" ")
+    for weight in range(height + 1):
+        print("*", end=" ")
+    print()
 
 
 print('\n\n*** Трикутник А ***')
@@ -25,6 +19,41 @@ for height in range(0, number, 2):
             if weight == 0:
                 string += '  *'
             elif weight <= number-2:
+                string += ' *'
+        elif number - height == weight or number + height == weight:
+            string += '*'
+        else:
+            string += ' '
+    print(string)
+print('\n\n\n*** Генерація чисел***')
+answer = input('Розпочати виконання програми? \nВведіть (y\\n): ')
+if answer == 'y':
+    my_string = '0123456789'
+    for i in my_string:
+        for j in my_string:
+            print(int(i+j))
+else:
+    print('Дякую за увагу')
+
+
+
+print('***Трикутник C ***')
+number = int(input('Введіть висоту трикутника: '))
+user_height = number * 2
+for height in range(0, user_height, 2):
+    for weigth in range(user_height - height):
+        print(end=" ")
+    for weight in range(height + 1):
+        print("*", end=" ")
+    print()
+number = user_height
+for height in range(number,-2, -2):
+    string = ''
+    for weight in range((number * 2) * 2):
+        if height == number:
+            if weight == 0:
+                string += '*'
+            elif weight <= number:
                 string += ' *'
         elif number - height == weight or number + height == weight:
             string += '*'
