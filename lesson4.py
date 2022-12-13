b69aab53871ac99db0c5f1cd1ded3165a73a5dcd
arulik <arulik@icloud.com>
2022-12-13 12:28:54 +0200

    first task

---
+++ lesson4.py
@@ -0,0 +1,17 @@
+print('***Трикутник B ***')
+number = int(input('Введіть висоту трикутника: '))
+user_height = number * 2
+for height in range(0, user_height, 2):
+    for weigth in range(user_height - height):
+        print(end=" ")
+    for weight in range(height + 1):
+        print("*", end=" ")
+    print()
+
+print('\n\n\n*** Генерація чисел***')
+my_string = '0123456789'
+for i in my_string:
+    for j in my_string:
+        print(int(i+j))
+
+test
