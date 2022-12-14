5e305dc0fc745f3ad44b0a444108460590a4147b
arulik <arulik@icloud.com>
2022-12-14 14:54:25 +0200

    lists dz done

--- Lesson5.py
+++ Lesson5.py
@@ -1,24 +1,22 @@
-# from random import randint
-#
-# my_list = [randint(1, 500) for i in range(300)]
-# for i in my_list:
-#     if i >= 100:
-#         print(i, end='\t')
-#
-#
-# my_list = [randint(1, 500) for i in range(300)]
-# my_results = []
-# for i in my_list:
-#     if i >= 100:
-#         my_results.append(i)
-# print(my_results)
-#
-# my_list = [randint(1, 5) for i in range(300)]
-# if len(my_list) <= 2:
-#     my_list.append(0)
-# elif len(my_list) >= 2:
-#     my_list.append(my_list[-1] + my_list[-2])
-# print(my_list)
+from random import randint
+
+my_list = [randint(1, 500) for i in range(300)]
+for i in my_list:
+    if i >= 100:
+        print(i, end='\t')
 
 
-my_list = [randint(1, 5) for i in range(300)]
+my_list = [randint(1, 500) for i in range(300)]
+my_results = []
+for i in my_list:
+    if i >= 100:
+        my_results.append(i)
+print(my_results)
+
+my_list = [randint(1, 5) for i in range(3)]
+if len(my_list) <= 2:
+    my_list.append(0)
+elif len(my_list) >= 2:
+    my_list.append(my_list[-1] + my_list[-2])
+print(my_list)
+
