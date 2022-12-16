407fdd972d2df2d44ce30bdaf17b6a5776ada198
arulik <arulik@icloud.com>
2022-12-16 11:19:20 +0200

    lists dz done

--- Lesson5.py
+++ Lesson5.py
@@ -1,18 +1,18 @@
 from random import randint
-
+print('*** Задача №1***')
 my_list = [randint(1, 500) for i in range(300)]
 for i in my_list:
     if i >= 100:
         print(i, end='\t')
 
-
+print('\n\n*** Задача №2***')
 my_list = [randint(1, 500) for i in range(300)]
 my_results = []
 for i in my_list:
     if i >= 100:
         my_results.append(i)
 print(my_results)
-
+print('\n\n*** Задача №3***')
 my_list = [randint(1, 5) for i in range(3)]
 if len(my_list) <= 2:
     my_list.append(0)
