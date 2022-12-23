daeb8d382260aedfa31c8d49eed89dfe38eb0932
arulik <arulik@icloud.com>
2022-12-23 20:20:32 +0200

    3,4,5,7,11,12,13

--- lesson6.py
+++ lesson6.py
@@ -5,6 +5,7 @@ from random import randint
 3. Даны списки my_list_1 и my_list_2. Создать список my_result в который вначале поместить элементы на четных местах из
 my_list_1, а потом все элементы на нечетных местах из my_list_2.
 '''
+print('\n*** Задача 3 ***\n')
 my_list = [randint(1, 500) for i in range(10)]
 my_list2 = [randint(1, 500) for i in range(10)]
 my_results = []
@@ -19,7 +20,7 @@ print(my_results)
 *4. Дан список my_list. СОЗДАТЬ НОВЫЙ список new_list у которого первый элемент из my_list стоит на последнем месте.
  Если my_list [1,2,3,4], то new_list [2,3,4,1]
 '''
-
+print('\n*** Задача 4 ***\n')
 my_list = [6, 7, 7, 1, 2, 3, 4]
 print(my_list)
 new_list = []
@@ -31,7 +32,7 @@ print(new_list)
 *5. Дан список my_list. В ЭТОМ списке первый элемент переставить на последнее место.[1,2,3,4] -> [2,3,4,1].
 Пересоздавать список нельзя! (используйте метод pop)
 '''
-
+print('\n*** Задача 5 ***\n')
 my_list = [randint(1, 500) for i in range(10)]
 print(my_list)
 my_list.append(my_list.pop(0))
@@ -42,15 +43,15 @@ print(my_list)
 *6. Дана строка в которой есть числа (разделяются пробелами). Например "43 больше чем 34, но меньше чем 56".
 Найти сумму ВСЕХ ЧИСЕЛ (А НЕ ЦИФР) в этой строке. Для данного примера ответ - 133.
 '''
-my_str = '43 больше чем 34, но меньше чем 56'
-my_str=my_str.split()
-print(my_str)
-my_list=[]
-for i in my_str:
-    if i.isdigit():
-        my_list.append(i)
-
-print(my_list)
+# my_str = '43 больше чем 34, но меньше чем 56'
+# my_str=my_str.split()
+# print(my_str)
+# my_list=[]
+# for i in my_str:
+#     if i.isdigit():
+#         my_list.append(i)
+#
+# print(my_list)
 
 
 '''
@@ -58,6 +59,7 @@ print(my_list)
 этой строке. Причем l_limit левее чем r_limit. В переменную sub_str поместить НАИБОЛЬШУЮ часть строки между этими
  символами. my_str = "My long string", l_limit = "o", r_limit = "g" -> sub_str = "ng strin".
 '''
+print('\n*** Задача 7 ***\n')
 my_str = 'My long string'
 l_limit = 'o'
 r_limit = 'g'
@@ -99,7 +101,7 @@ print(sub_str)
 '''*10. Дан список my_list в котором могут быть как строки (type str) так и целые числа (type int).
 Например [1, 2, 3, "11", "22", 33] Создать новый список в который поместить только строки из my_list
 '''
-
+print('\n*** Задача 10 ***\n')
 my_list = [1, 2, 3, "11", "22", 33]
 my_results = []
 for i in my_list:
@@ -111,7 +113,7 @@ print(my_results)
 11. Дана строка my_str. Создать список в который поместить те символы из my_str, которые встречаются
 в строке ТОЛЬКО ОДИН раз.
 '''
-
+print('\n*** Задача 11 ***\n')
 my_str = str('hello world')
 my_set = set(my_str)
 my_list = (list(my_set))
@@ -124,6 +126,7 @@ print(my_results)
 '''
 *12. Даны две строки. Создать список в который поместить те символы,которые есть в обеих строках хотя бы раз.
 '''
+print('\n*** Задача 12 ***\n')
 str1="aaaasdf1"
 str2="asdfff2"
 my_set1= set(str1)
@@ -135,7 +138,7 @@ print(list(my_set1)+list(my_set2))
  ТОЛЬКО ПО ОДНОМУ разу. Пример: для строк "aaaasdf1" и "asdfff2" ответ ["s", "d"],
   т.к. эти символы есть в каждой строке по одному разу
 '''
-
+print('\n*** Задача 13 ***\n')
 str1="aaaasdf1"
 str2="asdfff2"
 my_set =set(str1+str2)
