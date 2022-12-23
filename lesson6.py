bbfd437733a760c000cc120cb9a941e6adf22693
arulik <arulik@icloud.com>
2022-12-23 13:58:20 +0200

    new dz tests

---
+++ lesson6.py
@@ -0,0 +1,20 @@
+from random import randint
+
+# my_set = set([1,3,5,7,9,9])
+# print(my_set)
+
+
+# my_list = [randint(1, 500) for i in range(10)]
+# my_list2 = [randint(1, 500) for i in range(10)]
+# print(my_list)
+# print(my_list2)
+
+
+
+# my_list = [1,1,2,2,3,3,4,4,55,66,77,56]
+# my_set = set(my_list)
+# my_list2 =[]
+#     for i in my_set:
+#         my_list2.append(i)
+# print(my_list)
+# print(my_set)
