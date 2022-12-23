bf3d35acc2089c62a3c1ce6422ce3a878bd1cf2f
arulik <arulik@icloud.com>
2022-12-23 17:46:10 +0200

    3,4,5,7,11,12,13

--- lesson6.py
+++ lesson6.py
@@ -5,52 +5,52 @@ from random import randint
 3. Даны списки my_list_1 и my_list_2. Создать список my_result в который вначале поместить элементы на четных местах из
 my_list_1, а потом все элементы на нечетных местах из my_list_2.
 '''
-# my_list = [randint(1, 500) for i in range(10)]
-# my_list2 = [randint(1, 500) for i in range(10)]
-# my_results = []
-# print(my_list)
-# print(my_list2)
-# for i in range(0, len(my_list + 1)):
-#     my_results.append(my_list[i])
-#     my_results.append(my_list2[i])
-# print(my_results)
+my_list = [randint(1, 500) for i in range(10)]
+my_list2 = [randint(1, 500) for i in range(10)]
+my_results = []
+print(my_list)
+print(my_list2)
+for i in range(0, len(my_list)):
+    my_results.append(my_list[i])
+    my_results.append(my_list2[i])
+print(my_results)
 
 '''
 *4. Дан список my_list. СОЗДАТЬ НОВЫЙ список new_list у которого первый элемент из my_list стоит на последнем месте.
  Если my_list [1,2,3,4], то new_list [2,3,4,1]
 '''
 
-# my_list = [6, 7, 7, 1, 2, 3, 4]
-# print(my_list)
-# new_list = []
-# for i in range(1,len(my_list)):
-#     new_list.append(my_list[i])
-# new_list.append(my_list[0])
-# print(new_list)
+my_list = [6, 7, 7, 1, 2, 3, 4]
+print(my_list)
+new_list = []
+for i in range(1,len(my_list)):
+    new_list.append(my_list[i])
+new_list.append(my_list[0])
+print(new_list)
 '''
 *5. Дан список my_list. В ЭТОМ списке первый элемент переставить на последнее место.[1,2,3,4] -> [2,3,4,1].
 Пересоздавать список нельзя! (используйте метод pop)
 '''
 
-# my_list = [randint(1, 500) for i in range(10)]
-# print(my_list)
-# my_list.append(my_list.pop(0))
-# print(my_list)
+my_list = [randint(1, 500) for i in range(10)]
+print(my_list)
+my_list.append(my_list.pop(0))
+print(my_list)
 
 
 '''
 *6. Дана строка в которой есть числа (разделяются пробелами). Например "43 больше чем 34, но меньше чем 56".
 Найти сумму ВСЕХ ЧИСЕЛ (А НЕ ЦИФР) в этой строке. Для данного примера ответ - 133.
 '''
-# my_str = '43 больше чем 34, но меньше чем 56'
-# my_str=my_str.split()
-# print(my_str)
-# my_list=[]
-# for i in my_str:
-#     if i.isdigit():
-#         my_list.append(i)
-#
-# print(my_list)
+my_str = '43 больше чем 34, но меньше чем 56'
+my_str=my_str.split()
+print(my_str)
+my_list=[]
+for i in my_str:
+    if i.isdigit():
+        my_list.append(i)
+
+print(my_list)
 
 
 '''
@@ -83,9 +83,7 @@ print(sub_str)
  подчеркиванием ('_'). Примеры: 'abcd' -> ['ab', 'cd'], 'abcde' -> ['ab', 'cd', e_'](используйте срезы длинны 2)
 '''
 
-# my_str = 'abdce'
-# my_list =[]
-# for i in my_str:
+
 
 
 '''
@@ -102,35 +100,35 @@ print(sub_str)
 Например [1, 2, 3, "11", "22", 33] Создать новый список в который поместить только строки из my_list
 '''
 
-# my_list = [1, 2, 3, "11", "22", 33]
-# my_results = []
-# for i in my_list:
-#     if type(i) is str:
-#         my_results.append(i)
-# print(my_results)
+my_list = [1, 2, 3, "11", "22", 33]
+my_results = []
+for i in my_list:
+    if type(i) is str:
+        my_results.append(i)
+print(my_results)
 
 '''
 11. Дана строка my_str. Создать список в который поместить те символы из my_str, которые встречаются
 в строке ТОЛЬКО ОДИН раз.
 '''
 
-# my_str = str('hello world')
-# my_set = set(my_str)
-# my_list = (list(my_set))
-# my_results = []
-# for i in my_list:
-#     my_results.append(i)
-# print(my_results)
+my_str = str('hello world')
+my_set = set(my_str)
+my_list = (list(my_set))
+my_results = []
+for i in my_list:
+    my_results.append(i)
+print(my_results)
 
 
 '''
 *12. Даны две строки. Создать список в который поместить те символы,которые есть в обеих строках хотя бы раз.
 '''
-# str1="aaaasdf1"
-# str2="asdfff2"
-# my_set1= set(str1)
-# my_set2=set(str2)
-# print(list(my_set1)+list(my_set2))
+str1="aaaasdf1"
+str2="asdfff2"
+my_set1= set(str1)
+my_set2=set(str2)
+print(list(my_set1)+list(my_set2))
 
 '''
 *13. Даны две строки. Создать список в который поместить те символы, которые есть в обеих строках, но в каждой
@@ -138,7 +136,7 @@ print(sub_str)
   т.к. эти символы есть в каждой строке по одному разу
 '''
 
-# str1="aaaasdf1"
-# str2="asdfff2"
-# my_set =set(str1+str2)
-# print(list(my_set))
+str1="aaaasdf1"
+str2="asdfff2"
+my_set =set(str1+str2)
+print(list(my_set))
