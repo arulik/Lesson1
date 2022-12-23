1deaff2c242edf8a1e9555c825b9fe6287dff207
arulik <arulik@icloud.com>
2022-12-23 14:52:31 +0200

    third done

--- lesson6.py
+++ lesson6.py
@@ -6,20 +6,15 @@ from random import randint
 Даны списки my_list_1 и my_list_2. Создать список my_result в который вначале поместить элементы на четных местах из
 my_list_1, а потом все элементы на нечетных местах из my_list_2.
 '''
-my_list = [randint(1, 500) for i in range(10)]
-my_list2 = [randint(1, 500) for i in range(10)]
-my_results= []
-print(my_list)
-print(my_list2)
-for i in range(0,len(my_list)):
-    my_results.append(my_list[i])
-    my_results.append(my_list2[i])
-print(my_results)
-
-
-
-
-
+# my_list = [randint(1, 500) for i in range(10)]
+# my_list2 = [randint(1, 500) for i in range(10)]
+# my_results = []
+# print(my_list)
+# print(my_list2)
+# for i in range(0, len(my_list + 1)):
+#     my_results.append(my_list[i])
+#     my_results.append(my_list2[i])
+# print(my_results)
 
 # my_str = str('hello world')
 # my_set = set(my_str)
@@ -37,3 +32,16 @@ print(my_results)
 #        print(alist ," not uniq")
 #    else:
 #        chek.append(alist)
+
+'''
+*4. Дан список my_list. СОЗДАТЬ НОВЫЙ список new_list у которого первый элемент из my_list стоит на последнем месте.
+ Если my_list [1,2,3,4], то new_list [2,3,4,1]
+'''
+
+my_list = [6, 7, 7, 1, 2, 3, 4]
+print(my_list)
+new_list = []
+for i in range(1,len(my_list)):
+    new_list.append(my_list[i])
+new_list.append(my_list[0])
+print(new_list)
