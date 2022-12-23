ad1b2f7c2e70b33ec9c90b1b3dfbe34bddb06e7e
arulik <arulik@icloud.com>
2022-12-23 14:10:32 +0200

    first done

--- lesson6.py
+++ lesson6.py
@@ -2,29 +2,29 @@ from random import randint
 
 # my_set = set([1,3,5,7,9,9])
 # print(my_set)
-
-
-# my_list = [randint(1, 500) for i in range(10)]
-# my_list2 = [randint(1, 500) for i in range(10)]
-# print(my_list)
-# print(my_list2)
-
-
-
-# my_list = [1,1,2,2,3,3,4,4,55,66,77,56]
-# my_set = set(my_list)
-# my_list2 =[]
-#     for i in my_set:
-#         my_list2.append(i)
-# print(my_list)
-# print(my_set)
-
-
-my_str = str('hello world')
-my_set = set(my_str)
-my_list = (list(my_set))
-my_results = []
-# print(my_set)
+'''
+Даны списки my_list_1 и my_list_2. Создать список my_result в который вначале поместить элементы на четных местах из
+my_list_1, а потом все элементы на нечетных местах из my_list_2.
+'''
+my_list = [randint(1, 500) for i in range(10)]
+my_list2 = [randint(1, 500) for i in range(10)]
+my_results= []
+print(my_list)
+print(my_list2)
 for i in my_list:
     my_results.append(i)
+    for j in my_list2:
+        my_results.append(j)
 print(my_results)
+
+
+
+
+
+# my_str = str('hello world')
+# my_set = set(my_str)
+# my_list = (list(my_set))
+# my_results = []
+# for i in my_list:
+#     my_results.append(i)
+# print(my_results)
