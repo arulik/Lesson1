3df1dd1fbad271666abd247388953785e83b2754
arulik <arulik@icloud.com>
2023-01-09 12:21:31 +0200

    1-5 завдання

--- dz_functions_module.py
+++ dz_functions_module.py
@@ -30,6 +30,7 @@ def func_if_letter_in(my_list):
 
 
 def func_if_is_str(my_list_int_str):
+    ''' Якщо тип елементу == str вивести цей елемент в новий список'''
     my_result = []
     for word in my_list_int_str:
         if type(word) is str:
@@ -38,8 +39,19 @@ def func_if_is_str(my_list_int_str):
 
 
 def func_one_letter(my_str):
+    ''' Вести всі унікальні елементи із строки'''
     my_result = []
     for i in range(len(my_str)):
         if my_str.count(my_str[i]) == 1:
             my_result.append(my_str[i])
+    return my_result
+
+
+def func_one_time_str(my_str, my_str_2):
+    '''Вивести елементи 2х строк які зустрічаються хоча б один раз'''
+    my_result = []
+    for i in my_str:
+        for j in my_str_2:
+            if i == j:
+                my_result.append(i)
     return my_result
--- lesson10.py
+++ lesson10.py
@@ -1,9 +1,17 @@
 from dz_functions_module import *
 
-
 my_list = ['thomas', 'andrew', 'kris', 'antony']
 
 my_list_int_str = ['thomas', 'andrew', 'kris', 'antony', 20, 10, 5]
 
 my_str = 'abcdefghaabc'
+
+my_str_2 = 'abcde'
+
+my_result = []
+    for i in my_str:
+        for j in my_str_2:
+            if i == j:
+                my_result.append(i)
+    return my_result
 
