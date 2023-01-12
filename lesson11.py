2974a1dd10b3a3782831326df49cd7b5729177c3
arulik <arulik@icloud.com>
2023-01-12 13:16:13 +0200

    1-7 завдання

---
+++ lesson11.py
@@ -0,0 +1,15 @@
+def censore(filename, list):
+    with open(filename, "r") as file:
+        result = file.read()
+    for item in result:
+        for i in list:
+            censore_line = ''
+            for cline in range(len(i)):
+                censore_line += '*'
+            result = result.replace(i, censore_line)
+    with open('c:\\temp\censored.txt', "w") as file:
+        file.write(result)
+
+
+list = ['tist', 'tost', 'test']
+censore("c:/temp/lesson11.txt", list)
