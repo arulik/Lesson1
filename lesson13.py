5b4c3f86558cafb982c612eca6c1e3ee60ea0379
arulik <arulik@icloud.com>
2023-01-20 14:01:37 +0200

    DZ 13 classб готово

--- lesson13.py
+++ lesson13.py
@@ -18,6 +18,7 @@
 методы записи/чтения сотрудников в/из файла
 '''
 import json
+import csv
 
 
 class employee:
@@ -28,15 +29,25 @@ class employee:
     skills = ['ходить', "говорить", "кодить"]
     people_lang = ['Україньська', "Англійська"]
     coding_lang = ['Python', "C++", "lisp"]
+    card_emlpoyee = []
 
-    def __init__(self, firstname, lastname, age):
+    def set_emloyee(self, firstname, lastname, age):
+        '''
+        Введення початкових данних співробітника
+        :param firstname:
+        :param lastname:
+        :param age:
+        :return:
+        '''
         self.firstname = firstname
         self.lastname = lastname
         self.age = age
 
-
-
     def write_card_json_employer(self):
+        '''
+        Створення файлу json з данними співробітника
+        :return:
+        '''
         data = {}
         data['firstname'] = self.firstname
         data['lastname'] = self.lastname
@@ -49,43 +60,113 @@ class employee:
         with open(f'{self.firstname} {self.lastname}.json', "w", encoding='utf8') as file:
             json.dump(data, file, ensure_ascii=False)
 
+    def write_card_csv_employer(self):
+        '''
+        Створення файлу csv з данними співробітника
+        :return:
+        '''
+        data = {}
+        data['firstname'] = self.firstname
+        data['lastname'] = self.lastname
+        data['age'] = self.age
+        data['email'] = self.email
+        data['skills'] = self.skills
+        data['people_lang'] = self.people_lang
+        data['coding_lang'] = self.coding_lang
+
+        with open(f'{self.firstname} {self.lastname}.csv', "w", encoding='utf8') as file:
+            writer = csv.DictWriter(file, fieldnames=data.keys(), delimiter=';')
+            writer.writeheader()
+            writer.writerow(data)
+
+
+    def create_card_employer(self):
+        '''
+        Фунуція обёєднує всі властивості співробітника в 1 словник(для зручності)
+        :return:
+        '''
+        data = {}
+        data['firstname'] = self.firstname
+        data['lastname'] = self.lastname
+        data['age'] = self.age
+        data['email'] = self.email
+        data['skills'] = self.skills
+        data['people_lang'] = self.people_lang
+        data['coding_lang'] = self.coding_lang
+        self.card_eployee = data
+
     def set_email(self, email):
+        '''
+        Задати електронну пошту
+        :param email:
+        :return:
+        '''
         self.email = email
 
     def skills(self, skills_list):
+        '''
+        Задати вміння
+        :param skills_list:
+        :return:
+        '''
         self.skills = skills_list
 
     def check_skills(self, skill):
+        '''
+        Перевірка наявності вмінь у співробітника
+        :param skill:
+        :return:
+        '''
         if skill in self.skills:
             return True
         else:
             return False
 
     def get_list_employee(self):
+        '''
+        Отримати список співробітників з файлу json
+        :return:
+        '''
         filename = "list_employee.json"
         with open(filename, "r", encoding='utf8') as file:
             data = json.load(file)
         return data
 
     def add_employee_to_list(self):
+        '''
+        Додати співробітника до списку співробітників в файлі json
+        :return:
+        '''
         data = self.get_list_employee()
-        data.append(self.card_eployee)
-        with open("list_employee.json", "w", encoding='utf8') as file:
-            json.dump(data, file, ensure_ascii=False)
+        if self.search_employee_by_name_firstname(self.firstname, self.lastname):
+            print('Такий співробітник вже є')
+        else:
+            data.append(self.card_eployee)
+            with open("list_employee.json", "w", encoding='utf8') as file:
+                json.dump(data, file, ensure_ascii=False)
 
-    def serach_employee_by_name(self, name):
+    def search_employee_by_name_firstname(self, name, lastname):
+        '''
+        Знайти співробітника по імені та фамілії в списку співробітників
+        :param name:
+        :param lastname:
+        :return:
+        '''
         list_emp = []
         for emp in self.get_list_employee():
-            if name == emp['firstname']:
+            if name == emp['firstname'] and lastname == emp['lastname']:
                 list_emp.append(emp)
         return list_emp
 
 
-
-
-new_emp = employee('Ivan', 'Sidorov', 25)
+new_emp = employee()
 new_emp.set_email('test@test.com.ua')
 new_emp.skills(['something', 'something2'])
 new_emp.write_card_json_employer()
-print(new_emp.check_skills('something'))
-
+# print(new_emp.check_skills('something'))
+new_emp.create_card_employer()
+new_emp.add_employee_to_list()
+# print(new_emp.search_employee_by_name_firstname('Ivasik', 'Telesik'))
+for name in new_emp.get_list_employee():
+    print(name)
+new_emp.write_card_csv_employer()
